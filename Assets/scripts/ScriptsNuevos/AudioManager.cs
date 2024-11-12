5dd2972aadf28c70b7e515c1912a024893a044fe
ARTU5000 <126347072+ARTU5000@users.noreply.github.com>
2024-11-11 20:47:25 -0600

    sghsrh

---
+++ Assets/scripts/ScriptsNuevos/AudioManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance { get; private set; }
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject); // Destruye la nueva instancia si ya existe una
+            return;
+        }
+        
+        Instance = this;
+        DontDestroyOnLoad(gameObject); // Evita que se destruya al cambiar de escena
+        
+        audioSource = gameObject.AddComponent<AudioSource>();
+    }
+
+    public void PlayAudioClip(AudioClip clip, float volume = 1f)
+    {
+        audioSource.clip = clip;
+        audioSource.volume = volume;
+        audioSource.Play();
+    }
+
+    public void StopAudio()
+    {
+        audioSource.Stop();
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioSource.volume = volume;
+    }
+}
