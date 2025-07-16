1dab8b97f279ad49f743fdae12d6f9743530a2a0
ARTU5000 <126347072+ARTU5000@users.noreply.github.com>
2025-07-16 16:32:50 -0600

    menu pausa

--- Assets/scripts/Pausa.cs
+++ Assets/scripts/Pausa.cs
@@ -3,47 +3,75 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class Pausa : MonoBehaviour
 {
     public Text instructions;
     private bool pause;
+    
+    public GameObject boton;
 
     // Start is called before the first frame update
     void Start()
     {
         pause = false;
+        instructions.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && pause == false)
+        if (Input.GetButtonDown("Pause") && pause == false)
         {
+            
+            instructions.gameObject.SetActive(true);
             pause = true;
             Time.timeScale = 0f;
-            instructions.text = "oprime R para repetir patrida                oprime M para regresar al men�";
+            instructions.text = "Pausa";
+            EventSystem.current.SetSelectedGameObject(null);
 
+            EventSystem.current.SetSelectedGameObject(boton);
         }
-        else if (Input.GetKey(KeyCode.R) && pause == true)
-        {
-
-            pause = false;
-            Time.timeScale = 1f;
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        }
-        else if (Input.GetKey(KeyCode.M) && pause == true)
-        {
-
-            pause = false;
-            Time.timeScale = 1f;
-            SceneManager.LoadScene("Menu");
-        }
-        else if (Input.GetKeyDown(KeyCode.Space) && pause == true)
+        else if (Input.GetButtonDown("Pause") && pause == true)
         {
             pause = false;
             instructions.text = " ";
             Time.timeScale = 1f;
+            
+            instructions.gameObject.SetActive(false);
+        }
+    }
+
+    public void unPause()
+    {
+        if (pause == true)
+        {
+            pause = false;
+            instructions.text = " ";
+            Time.timeScale = 1f;
+            
+            instructions.gameObject.SetActive(false);
+        }
+    }
+
+    public void Reload()
+    {
+        if (pause == true)
+        {
+            pause = false;
+            Time.timeScale = 1f;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    public void MainMenu()
+    {
+        if (pause == true)
+        {
+            pause = false;
+            Time.timeScale = 1f;
+            SceneManager.LoadScene("Menu");
         }
     }
 }
